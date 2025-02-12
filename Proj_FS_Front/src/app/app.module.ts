a43ffa12cc310c5361801ca0b18a780ecca3eb87
Wassil <wnakib21@gmail.com>
2025-02-12 22:47:14 +0100

    recherche dynamique

--- Proj_FS_Front/src/app/app.module.ts
+++ Proj_FS_Front/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
-import { ListeCentresComponent } from './features/reservations/liste-centres/liste-centres.component';
 import { RouterModule } from '@angular/router';
-import { ButtonShowListCentersComponent } from './features/reservations/button-show-list-centers/button-show-list-centers.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppComponent, // Importation du module de recherche
-    ListeCentresComponent, // Importation du module de liste des centres
     RouterModule.forRoot(routes), // Importation des routes
-    ButtonShowListCentersComponent // Importation du module de bouton pour afficher la liste des centres
   ],
   providers: [],
   bootstrap: []
--- Proj_FS_Front/src/app/app.routes.ts
+++ Proj_FS_Front/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import {AdminMenuComponent} from './features/admins/admin-menu/admin-menu.component';
 
 export const routes: Routes = [
-   
+    { path: 'admin/menu', component: AdminMenuComponent },
 ];
