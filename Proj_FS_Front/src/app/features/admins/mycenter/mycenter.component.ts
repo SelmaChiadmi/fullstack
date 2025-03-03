589a0248728827e376d931d9560933f8b6af6c61
Wassil <wnakib21@gmail.com>
2025-03-03 22:20:39 +0100

    form medecins

--- Proj_FS_Front/src/app/features/admins/mycenter/mycenter.component.ts
+++ Proj_FS_Front/src/app/features/admins/mycenter/mycenter.component.ts
@@ -10,6 +10,40 @@ import { CommonModule } from '@angular/common';
   styleUrl: './mycenter.component.css'
 })
 export class MycenterComponent {
+  // add-employee.component.ts
+
+  isOpen = false;
+  formData = {
+    nom: '',
+    prenom: '',
+    email: '',
+    password: '',
+    centre: ''
+  };
+
+  openForm() {
+    this.isOpen = true;
+  }
+
+  closeForm() {
+    this.isOpen = false;
+    this.resetForm();
+  }
+
+  onSubmit() {
+    console.log('Nouvel employé :', this.formData);
+    this.closeForm(); // Ferme le formulaire après soumission
+  }
+
+  private resetForm() {
+    this.formData = {
+      nom: '',
+      prenom: '',
+      email: '',
+      password: '',
+      centre: ''
+    };
+  }
 
   isConfirmationDisplayed: boolean = false;
      // Fonction pour afficher le message de confirmation
