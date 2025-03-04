e2de490ee448068bc26e9489887360c8b19bdc9d
19939776 <salma.chiadmi@metapolis.fr>
2025-03-04 01:05:23 +0100

    now an admin can get the info of his center

--- Proj_FS_Back/src/main/java/com/Vaccination/projet/services/CentreService.java
+++ Proj_FS_Back/src/main/java/com/Vaccination/projet/services/CentreService.java
@@ -3,22 +3,32 @@ package com.Vaccination.projet.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.Vaccination.projet.Repositories.CentreRepository;
+import com.Vaccination.projet.Repositories.EmployesRepo;
 import com.Vaccination.projet.dto.updateCentreDto;
 import com.Vaccination.projet.entities.centres;
+import com.Vaccination.projet.entities.employes;
 
 @Service
 public class CentreService {
     
     @Autowired
     private CentreRepository centrerepo;
+    @Autowired
+    private EmployesRepo employerepo;
+   
 
-    public CentreService(CentreRepository centrerepo) {
+
+
+    public CentreService(CentreRepository centrerepo, EmployesRepo employesRepo) {
         this.centrerepo = centrerepo;
+        this.employerepo = employesRepo;
+        
     }
 
     public List<centres> getAllCentres(){
@@ -31,8 +41,10 @@ public class CentreService {
 
     }
 
-    public centres getCentreById(int id){
-        return centrerepo.findCentreById(id);
+    public centres getCentreById(int centreId){
+       
+        
+        return centrerepo.findCentreById(centreId);
     }
 
     public void addCentre(centres centre){
@@ -57,8 +69,17 @@ public class CentreService {
         centrerepo.save(centreToUpdate);
     }
 
-   
+    public int getLoggedInUserCentreId() {
+        
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+    
 
+        employes employe = employerepo.findByMail(username)
+                .orElseThrow(() -> new RuntimeException("Utilisateur non trouvé"));
+        
+     
+        return employe.getCentre().getId(); 
+    }
     
 
 }
